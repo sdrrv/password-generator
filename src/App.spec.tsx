5facc269c8bb085dbb3aa3921f04832e690ff94f
Duarte Elvas <duartecelvas@gmail.com>
2022-09-26 14:56:28 +0100

    Config/add tests (#4)
    * Fiz Lint errors
    
    * Add vitest
    
    * Vitest added

---
+++ src/App.spec.tsx
@@ -0,0 +1,13 @@
+import { describe, it, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App.tsx", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it("should render", async () => {
+    await screen.getByText("Hello World");
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
+
+// @ts-ignore
+  test: {
+    globals: true,
+    environment: "jsdom",
+    deps: {
+      registerNodeLoader: false,
+    },
+  },
 });
