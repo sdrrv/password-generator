59d5fc30941f278c6e18376792a290745847944a
sdrrv <duartecelvas@gmail.com>
2022-09-26 18:58:43 +0100

    Init Password generator created

---
+++ src/utils/passwordGenerator.ts
@@ -0,0 +1,10 @@
+function passwordGenerator(length: number, chars: string) {
+  let result = "";
+  const charsLength = chars.length;
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * charsLength));
+  }
+  return result;
+}
+
+export { passwordGenerator };
