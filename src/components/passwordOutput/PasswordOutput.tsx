6646605908da1209f132b88ad2a55b3ebafe204b
sdrrv <duartecelvas@gmail.com>
2022-09-27 16:24:22 +0100

    PasswordStrenght indicator

--- src/components/passwordOutput/PasswordOutput.tsx
+++ src/components/passwordOutput/PasswordOutput.tsx
@@ -5,6 +5,7 @@ function PasswordOutput() {
   return (
     <div className="pass-output">
       <div className="pass-output__text">Hello World</div>
+      <div className="pass-strength strong" />
     </div>
   );
 }
