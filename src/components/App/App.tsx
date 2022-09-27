17ec4b566ccba22dd0c0cb310ea891fea99f006a
Duarte Elvas <duartecelvas@gmail.com>
2022-09-27 16:26:36 +0100

    Feature/password output (#8)
    * Removed App
    
    * PasswordOutput.tsx file created
    
    * PasswordOutput init
    
    * PasswordStrenght indicator

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
 import "./App.component.scss";
+import PasswordOutput from "../passwordOutput/PasswordOutput";
 
 function App() {
   return (
     <div className="resize">
-      <h1>Hello World</h1>
+      <PasswordOutput />
     </div>
   );
 }
---
+++ src/components/passwordOutput/PasswordOutput.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./passwordOutput.component.scss";
+
+function PasswordOutput() {
+  return (
+    <div className="pass-output">
+      <div className="pass-output__text">Hello World</div>
+      <div className="pass-strength strong" />
+    </div>
+  );
+}
+
+export default PasswordOutput;
