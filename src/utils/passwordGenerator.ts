15951442ae550e4351c59c7029b01dc6c35504af
sdrrv <duartecelvas@gmail.com>
2022-09-30 12:38:30 +0100

    Shuffle Funtion created

--- src/utils/passwordGenerator.ts
+++ src/utils/passwordGenerator.ts
@@ -1,4 +1,34 @@
-function passwordGenerator(length: number, chars: string) {
+enum SelectedUses {
+  UpperCase,
+  LowerCase,
+  Symbols,
+  Numbers,
+}
+
+const upperCases = "ABCDEFGHIJKLMNOPQRSTUVXZ";
+const lowerCases = upperCases.toLowerCase();
+const symbols = '!"#$%&/()=@€+*,';
+const numbers = "0123456789";
+
+function shuffle(str: string): string {
+  return str
+    .split("")
+    .sort(function () {
+      return 0.5 - Math.random();
+    })
+    .join("");
+}
+
+function generate(selectedUses: SelectedUses): string {
+  let result = "";
+  if (selectedUses === SelectedUses.UpperCase) result += upperCases;
+  if (selectedUses == SelectedUses.LowerCase) result += lowerCases;
+  if (selectedUses === SelectedUses.Symbols) result += symbols;
+  if (selectedUses === SelectedUses.Numbers) result += numbers;
+  return shuffle(result);
+}
+
+function passwordGenerator(length: number, chars: string): string {
   let result = "";
   const charsLength = chars.length;
   for (let i = 0; i < length; i++) {
@@ -7,4 +37,4 @@ function passwordGenerator(length: number, chars: string) {
   return result;
 }
 
-export { passwordGenerator };
+export { passwordGenerator, generate, shuffle };
