e533ed98ded32cfc4c95201ec369b24831790520
sdrrv <duartecelvas@gmail.com>
2022-09-30 14:48:17 +0100

    Password generator tested

--- src/utils/tests/passwordGenerator.spec.ts
+++ src/utils/tests/passwordGenerator.spec.ts
@@ -1,5 +1,14 @@
 import { describe, expect, it } from "vitest";
-import { generate, SelectedUses, shuffle } from "../passwordGenerator";
+import {
+  generate,
+  passwordGenerator,
+  SelectedUses,
+  shuffle,
+} from "../passwordGenerator";
+
+function randomNumberFromInterval(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 describe("shuffle", () => {
   it("should shuffle", () => {
@@ -14,5 +23,30 @@ describe("generate", () => {
     expect(output.length).toEqual(52);
     expect(output.includes("A")).toBe(true);
     expect(output.includes("a")).toBe(true);
+  });
+
+  it("should generate a string with the available chars with !#$ & 1234 & ABC", () => {
+    const output = generate([
+      SelectedUses.Symbols,
+      SelectedUses.Numbers,
+      SelectedUses.UpperCase,
+    ]);
+    expect(output.length).toEqual(51);
+    expect(output.includes(")")).toBe(true);
+    expect(output.includes("9")).toBe(true);
+    expect(output.includes("A")).toBe(true);
+    expect(output.includes("a")).toBe(false);
+  });
+});
+
+describe("passwordGenerator", () => {
+  it("should generate a password with length X", () => {
+    const length = randomNumberFromInterval(1, 20);
+    expect(
+      passwordGenerator(
+        length,
+        generate([SelectedUses.UpperCase, SelectedUses.LowerCase])
+      ).length
+    ).toEqual(length);
   });
 });
