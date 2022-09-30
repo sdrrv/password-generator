bb21337b7091cd74d5682342ee0b5deab8519fe4
sdrrv <duartecelvas@gmail.com>
2022-09-30 15:06:17 +0100

    Options component created

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import "./App.component.scss";
 import PasswordOutput from "../passwordOutput/PasswordOutput";
+import Options from "../options/Options";
 
 function App() {
   return (
     <div className="resize">
       <PasswordOutput />
+      <Options />
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import eslint from "vite-plugin-eslint";
 export default defineConfig({
   plugins: [react(), eslint()],
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   test: {
     globals: true,
