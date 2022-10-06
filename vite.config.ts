c862104a211d5663a5dfc07ea6d2d1b47058ef05
Duarte Elvas <duartecelvas@gmail.com>
2022-10-06 12:16:09 +0100

    Bootstrap Icons added (#9)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,14 @@ export default defineConfig({
     environment: "jsdom",
     deps: {
       registerNodeLoader: false,
+    },
+  },
+  resolve: {
+    alias: {
+      "~bootstrap-icons": path.resolve(
+        __dirname,
+        "node_modules/bootstrap-icons"
+      ),
     },
   },
 });
