4289aacaca1e22ebec565ddc93bcb2a9dbef0425
sdrrv <duartecelvas@gmail.com>
2022-10-06 13:16:09 +0100

    test output Buttons added

--- src/components/passwordOutput/PasswordOutput.tsx
+++ src/components/passwordOutput/PasswordOutput.tsx
@@ -8,6 +8,14 @@ function PasswordOutput({ password }: Props) {
   return (
     <div className="pass-output">
       <div className="pass-output__text">{password}</div>
+      <div className="pass-output__buttons">
+        <button>
+          <i className="bi bi-arrow-clockwise" />
+        </button>
+        <button>
+          <i className="bi bi-clipboard" />
+        </button>
+      </div>
       <div className="pass-strength strong" />
     </div>
   );
